5659bd97b421fdab96d2fac032984a482ea33907
Subhash Bhushan <subhash.bhushan@gmail.com>
2019-04-24 07:31:52 -0700

    Upgrade protean version to 0.0.11

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     ],
     install_requires=[
         'click==7.0',
-        'protean==0.0.10',
+        'protean==0.0.11',
         'sqlalchemy==1.3.3'
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
